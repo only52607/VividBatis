61742cb2efd16b30b91fcda6b52a6f6edc8677fd
GitHub Action <action@github.com>
2025-08-02 02:53:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/only52607/vividbatis/MyBundle.kt
+++ src/main/kotlin/com/github/only52607/vividbatis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.only52607.vividbatis
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/only52607/vividbatis/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/only52607/vividbatis/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.only52607.vividbatis.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
